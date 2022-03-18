581a106f8267b9ce2898a96fba6664d807236c9a
Aruzo <dawid12391111@gmail.com>
2022-03-18 20:13:02 +0100

    Added pokemons fetching on scroll.

--- components/Pokemons.tsx
+++ components/Pokemons.tsx
@@ -1,28 +1,30 @@
+import { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_POKEMONS, GetPokemonsQuery } from "../graphql/queries";
 import Pokemon from "../components/Pokemon";
 
 const Pokemons = () => {
-  const { loading, error, data, fetchMore } = useQuery<GetPokemonsQuery>(
+  const { data, fetchMore } = useQuery<GetPokemonsQuery>(
     GET_POKEMONS,
     { variables: { offset: 0 } }
   );
 
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error</h1>;
+  const fetchMoreIfBottom = () => {
+    if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+      fetchMore({ variables: { offset: data!.pokemons.length } });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", fetchMoreIfBottom);
+    return () => window.removeEventListener("scroll", fetchMoreIfBottom);
+  }, [data]);
 
   return (
     <div className="container grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 my-8">
-      {data!.pokemons.map((pokemon) => (
+      {data?.pokemons.map((pokemon) => (
         <Pokemon key={pokemon.id} pokemon={pokemon} />
       ))}
-      <button
-        onClick={() =>
-          fetchMore({ variables: { offset: data!.pokemons.length } })
-        }
-      >
-        Fetch More
-      </button>
     </div>
   );
 };
