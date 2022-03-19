090e39ea1e6d03237b688692483b6c6ec53defb7
Aruzo <dawid12391111@gmail.com>
2022-03-19 16:57:27 +0100

    Added Home title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Pokemons from "../components/Pokemons";
 
 const Home: NextPage = () => {
-  return <Pokemons />;
+  return (
+    <main>
+      <Head>
+        <title>Pokedex | Home</title>
+      </Head>
+      <Pokemons />
+    </main>
+  );
 };
 
 export default Home;
