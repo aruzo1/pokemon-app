e5cf8c08cd1c1a9426f223f93cc115eb82424203
Aruzo <dawid12391111@gmail.com>
2022-03-20 20:40:15 +0100

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="en">
-      <Head>
-        
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Pokemons from "../components/Pokemons";
-import thumbnail from "../public/thumbnail.jpg";
 
 const Home: NextPage = () => {
   return (
     <main>
       <Head>
         <title>Pokedex | Home</title>
+        <meta property="og:type" content="website" />
         <meta property="og:image" content="/thumbnail.jpg" />
-        <meta property="og:image:width" content={`${thumbnail.width}`} />
-        <meta property="og:image:height" content={`${thumbnail.height}`} />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
       </Head>
       <Pokemons />
     </main>
