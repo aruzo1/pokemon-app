efc83f8bc9867313923073e819cc0c109cade7c7
Aruzo <dawid12391111@gmail.com>
2022-03-20 19:06:44 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Pokemons from "../components/Pokemons";
+import x from "../public/thumbnail.png";
 
 const Home: NextPage = () => {
   return (
     <main>
       <Head>
         <title>Pokedex | Home</title>
+        <meta property="og:image" content={x.src} />
+        <meta property="og:image:height" content={x.height.toString()} />
+        <meta property="og:image:width" content={x.width.toString()} />
       </Head>
       <Pokemons />
     </main>
