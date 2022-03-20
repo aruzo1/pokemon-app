0a2589ac664eed14fa33adc88f788e2571b28a62
Aruzo <dawid12391111@gmail.com>
2022-03-20 20:22:29 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,24 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
 import Pokemons from "../components/Pokemons";
 import thumbnail from "../public/thumbnail.png";
 
-const Home: NextPage<{ host: string }> = ({ host }) => {
+const Home: NextPage = () => {
   return (
     <main>
       <Head>
         <title>Pokedex | Home</title>
         <meta property="og:type" content="website" />
-        <meta property="og:image" content={"https://" + host + thumbnail.src} />
+        <meta
+          property="og:image"
+          content={"https://pokedex-liard-one.vercel.app/" + thumbnail.src}
+        />
         <meta property="og:image:width" content={`${thumbnail.width}`} />
         <meta property="og:image:height" content={`${thumbnail.height}`} />
       </Head>
       <Pokemons />
     </main>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  return { props: { host: req.headers.host } };
 };
 
 export default Home;
