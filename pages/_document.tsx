6d4109fcc644ba4b0a3ddf1428b37719c039cf61
Aruzo <dawid12391111@gmail.com>
2022-03-20 18:48:05 +0100

    Fixed _document.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { Html, Head, Main, NextScript } from "next/document";
+import React, { ReactElement } from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default () => {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta
-          name="description"
-          content="The Pokédex is an electronic device created and designed to catalog and provide information regarding the various species of Pokémon featured in the Pokémon video game, anime and manga series."
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+export default class MyDocument extends Document {
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="The Pokédex is an electronic device created and designed to catalog and provide information regarding the various species of Pokémon featured in the Pokémon video game, anime and manga series."
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
