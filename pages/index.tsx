c673e9374be45c8a5d0b29e1c6fc1f22e7c6a415
Aruzo <dawid12391111@gmail.com>
2022-03-20 19:33:58 +0100

    Changed meta file.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,6 @@ const Home: NextPage = () => {
     <main>
       <Head>
         <title>Pokedex | Home</title>
-        <meta
-          property="og:image"
-          content="https://pokedex-liard-one.vercel.app/thumbnail.png"
-        />
-        <meta property="og:image:width" content="1920" />
-        <meta property="og:image:height" content="1080" />
       </Head>
       <Pokemons />
     </main>
