0b19bdb2a781955622faf39af3968eeb9802be71
Aruzo <dawid12391111@gmail.com>
2022-03-20 19:06:44 +0100

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,6 @@ const Document = () => {
           content="The Pokédex is an electronic device created and designed to catalog and provide information regarding the various species of Pokémon featured in the Pokémon video game, anime and manga series."
         />
         <meta name="author" content="Aruzo" />
-        <meta
-          property="og:image"
-          content="/thumbnail.png"
-        />
-        <meta property="og:image:height" content="1920" />
-        <meta property="og:image:width" content="1080" />
       </Head>
       <body>
         <Main />
