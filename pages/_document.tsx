66e7ff2121fe622e0e5378da58c3edc681d657d2
Aruzo <dawid12391111@gmail.com>
2022-03-20 18:48:05 +0100

    Fixed _document.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default () => {
+const Document = () => {
   return (
     <Html lang="en">
       <Head>
@@ -17,3 +17,5 @@ export default () => {
     </Html>
   );
 };
+
+export default Document;
