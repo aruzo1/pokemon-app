c4fb2d7a1ceaf53a0093fd1d9246b8a387517ded
Aruzo <dawid12391111@gmail.com>
2022-03-20 20:22:39 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <meta property="og:type" content="website" />
         <meta
           property="og:image"
-          content={"https://pokedex-liard-one.vercel.app/" + thumbnail.src}
+          content={"https://pokedex-liard-one.vercel.app" + thumbnail.src}
         />
         <meta property="og:image:width" content={`${thumbnail.width}`} />
         <meta property="og:image:height" content={`${thumbnail.height}`} />
