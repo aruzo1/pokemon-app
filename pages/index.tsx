f03040b74bba6e32a237a60924946cd7cd17505d
Aruzo <dawid12391111@gmail.com>
2022-03-20 20:32:57 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Pokemons from "../components/Pokemons";
+import thumbnail from "../public/thumbnail.png";
 
 const Home: NextPage = () => {
   return (
     <main>
       <Head>
         <title>Pokedex | Home</title>
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://pokedex-liard-one.vercel.app/thumbnail.png"
-        />
-        <meta property="og:image:width" content="1920" />
-        <meta property="og:image:height" content="1080" />
+        <meta property="og:image" content={thumbnail.src} />
+        <meta property="og:image:width" content={`${thumbnail.width}`} />
+        <meta property="og:image:height" content={`${thumbnail.height}`} />
       </Head>
       <Pokemons />
     </main>
