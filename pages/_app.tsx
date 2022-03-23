a18150ef46707efe8f3a5b48a1ba57bf5a759389
Aruzo <dawid12391111@gmail.com>
2022-03-23 16:50:18 +0100

    Changed Chakra UI to TailwindCSS.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import theme from "../styles/theme";
+import "../styles/globals.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,9 +17,7 @@ function PokedexApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
+        <Component {...pageProps} />
       </Hydrate>
     </QueryClientProvider>
   );
