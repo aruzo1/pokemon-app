b0d19ff54f7b7f23130b7bdec9a81d9a1135f05c
Aruzo <dawid12391111@gmail.com>
2022-03-25 11:20:44 +0100

    Added checkboxes.

--- components/home/TypesMenu.tsx
+++ components/home/TypesMenu.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import Check from "../../public/icons/check.svg";
+
+const Checkbox = ({ label }: { label: string }) => {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div className="flex items-center">
+      <button
+        onClick={() => setChecked((prev) => !prev)}
+        className={`center w-8 h-8 bor rounded-lg ${
+          checked ? "bg-gray-800" : "bg-gray-900"
+        }`}
+      >
+        {checked && <Check />}
+      </button>
+      <label className="ml-4 font-medium">{label}</label>
+    </div>
+  );
+};
+
 const TypesMenu = () => {
   return (
-    <div className="col-span-1">
+    <div className="col-span-1 flex flex-col gap-y-4">
       <h3 className="font-bold text-2xl">Types</h3>
-      <input type="checkbox" />
+      <Checkbox label="Fire" />
+      <Checkbox label="Water" />
+      <Checkbox label="Grass" />
     </div>
   );
 };
