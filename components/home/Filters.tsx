1003865ec3d7d0d394ef60634b7967581977463a
Aruzo <dawid12391111@gmail.com>
2022-03-26 22:57:14 +0100

    ,

--- components/home/Filters.tsx
+++ components/home/Filters.tsx
@@ -10,25 +10,25 @@ const Filters = (props: {
 }) => {
   const { filters, dispatch } = props;
 
+  const content = (
+    <TypesMenu
+      types={filters.types}
+      addType={(payload) => dispatch({ type: "ADD_TYPE", payload })}
+      removeType={(payload) => dispatch({ type: "REMOVE_TYPE", payload })}
+    />
+  );
+
   return (
     <div className="col-span-1 order-1 lg:order-2">
       <div className="hidden lg:block p-4 bor rounded-lg bg-gray-800">
-        <TypesMenu
-          types={filters.types}
-          addType={(payload) => dispatch({ type: "ADD_TYPE", payload })}
-          removeType={(payload) => dispatch({ type: "REMOVE_TYPE", payload })}
-        />
+        {content}
       </div>
       <Disclosure>
         <Disclosure.Button className="lg:hidden p-4 bor rounded-lg bg-gray-800">
           <Filter />
         </Disclosure.Button>
         <Disclosure.Panel className="lg:hidden overflow-y-auto z-10 fixed top-0 left-0 w-full h-full p-4 bg-gray-800">
-          <TypesMenu
-            types={filters.types}
-            addType={(payload) => dispatch({ type: "ADD_TYPE", payload })}
-            removeType={(payload) => dispatch({ type: "REMOVE_TYPE", payload })}
-          />
+          {content}
           <Disclosure.Button className="w-full mt-4 py-2 bor rounded-lg font-bold">
             Show pokemons
           </Disclosure.Button>
